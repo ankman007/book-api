acd01a329208943a6e0ddfbe4bc5527f1bcf12ef
Ankit Poudel <108212023+ankman007@users.noreply.github.com>
2025-07-27 22:28:38 +0545

    fixes: published_date variable name fix

--- books/api/serializers.py
+++ books/api/serializers.py
@@ -4,7 +4,7 @@ from .models import Book
 class BookSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=500)
     author = serializers.CharField(max_length=255)
-    published = serializers.DateField()
+    published_date = serializers.DateField()
     
     def create(self, validated_data):
         return Book.objects.create(**validated_data)
