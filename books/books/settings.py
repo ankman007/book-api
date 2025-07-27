eba8496c055f70c330066c634a43e973f8f137ca
Ankit Poudel <108212023+ankman007@users.noreply.github.com>
2025-07-27 12:42:15 +0545

    feat: add auto Swagger API documentation generation code

--- books/books/settings.py
+++ books/books/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- books/books/urls.py
+++ books/books/urls.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+     openapi.Info(
+        title="Book API",
+        default_version='v1',
+        description="Simple Book API built using Django REST Framework",
+    ),
+    public=True, 
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', include('api.urls')),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
